6b8c7a7bfb7e27db8ed493103af1ac18575025c4
blast-sky <strogancev2020@gmail.com>
2023-07-05 23:34:33 +0300

    add controller unit test

---
+++ src/test/kotlin/com/astrog/subscriptionservice/configuration/StringToSubscriptionTypeConverterTest.kt
@@ -0,0 +1,42 @@
+package com.astrog.subscriptionservice.configuration
+
+import com.astrog.subscriptionservice.model.domain.SubscriptionType
+import com.astrog.subscriptionservice.model.exception.UnsupportedSubscriptionTypeException
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class StringToSubscriptionTypeConverterTest {
+
+    private val converter = StringToSubscriptionTypeConverter()
+
+    @ParameterizedTest
+    @MethodSource("provideStringAndSubscriptionType")
+    fun `convert Must return right SubscriptionType When take right argument`(
+        source: String,
+        expected: SubscriptionType,
+    ) {
+        assertEquals(expected, converter.convert(source))
+    }
+
+    @Test
+    fun `convert Must throw UnsupportedSubscriptionType When source is not valid`() {
+        val notValidSubscriptionType = "mms"
+        assertThrows<UnsupportedSubscriptionTypeException> { converter.convert(notValidSubscriptionType) }
+    }
+
+    companion object {
+
+        @JvmStatic
+        private fun provideStringAndSubscriptionType(): List<Arguments> {
+            return listOf(
+                Arguments.of("telegram", SubscriptionType.TELEGRAM),
+                Arguments.of("sms", SubscriptionType.SMS),
+                Arguments.of("email", SubscriptionType.EMAIL),
+            )
+        }
+    }
+}
---
+++ src/test/kotlin/com/astrog/subscriptionservice/controller/SubscriptionControllerTest.kt
@@ -0,0 +1,217 @@
+package com.astrog.subscriptionservice.controller
+
+import com.astrog.subscriptionservice.model.domain.SubscriptionType
+import com.astrog.subscriptionservice.model.exception.SubscriptionAlreadyExistException
+import com.astrog.subscriptionservice.randomCreateSubscriptionDto
+import com.astrog.subscriptionservice.randomRemoveSubscriptionDto
+import com.astrog.subscriptionservice.service.SubscriptionService
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.verify
+import org.hamcrest.CoreMatchers.containsString
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.TestConstructor
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.delete
+import org.springframework.test.web.servlet.post
+
+@WebMvcTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+class SubscriptionControllerTest(
+    private val mockMvc: MockMvc,
+    private val objectMapper: ObjectMapper,
+) {
+
+//    @TestConfiguration
+//    class PathVariableBinder : WebMvcConfigurer {
+//
+//        override fun addFormatters(registry: FormatterRegistry) {
+//            registry.addConverter(StringToSubscriptionTypeConverter())
+//        }
+//    }
+
+
+    @MockkBean
+    private lateinit var subscriptionService: SubscriptionService
+
+    @Nested
+    inner class CreateTest {
+
+        @Test
+        fun `create Must return ok When ok`() {
+            val createSubscriptionDto = randomCreateSubscriptionDto
+
+            every {
+                subscriptionService.createSubscription(
+                    userId = createSubscriptionDto.userId,
+                    subscriptionType = createSubscriptionDto.subscriptionType,
+                    filters = createSubscriptionDto.filters,
+                )
+            } returns Unit
+
+            mockMvc.post("/subscriptions/create") {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(createSubscriptionDto)
+            }
+                .andDo { print() }
+                .andExpectAll {
+                    status { isOk() }
+                }
+
+            verify {
+                subscriptionService.createSubscription(
+                    userId = createSubscriptionDto.userId,
+                    subscriptionType = createSubscriptionDto.subscriptionType,
+                    filters = createSubscriptionDto.filters,
+                )
+            }
+        }
+
+        @Test
+        fun `create Must return CONFLICT When subscription already exist`() {
+            val createSubscriptionDto = randomCreateSubscriptionDto
+
+            every {
+                subscriptionService.createSubscription(
+                    userId = createSubscriptionDto.userId,
+                    subscriptionType = createSubscriptionDto.subscriptionType,
+                    filters = createSubscriptionDto.filters,
+                )
+            } throws SubscriptionAlreadyExistException(
+                createSubscriptionDto.userId,
+                createSubscriptionDto.subscriptionType,
+            )
+
+            mockMvc.post("/subscriptions/create") {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(createSubscriptionDto)
+            }
+                .andDo { print() }
+                .andExpectAll {
+                    status { isConflict() }
+                }
+
+            verify {
+                subscriptionService.createSubscription(
+                    userId = createSubscriptionDto.userId,
+                    subscriptionType = createSubscriptionDto.subscriptionType,
+                    filters = createSubscriptionDto.filters,
+                )
+            }
+        }
+
+        @Test
+        fun `create Must return BAD_REQUEST When subscription type is not valid`() {
+            val notValidContent = """
+                {
+                    "userId": "123",
+                    "type": "mms",
+                    "filters": []
+                }
+            """.trimIndent()
+
+            val expectedResult =
+                "JSON parse error: Cannot deserialize value of type `SubscriptionType` from String \"mms\": not one of the values accepted for Enum class: [sms, email, telegram]"
+
+            mockMvc.post("/subscriptions/create") {
+                contentType = MediaType.APPLICATION_JSON
+                content = notValidContent
+            }
+                .andDo { print() }
+                .andExpectAll {
+                    status { isBadRequest() }
+                    content { string(expectedResult) }
+                }
+
+            verify(exactly = 0) {
+                subscriptionService.createSubscription(
+                    userId = any(),
+                    subscriptionType = any(),
+                    filters = any(),
+                )
+            }
+        }
+
+        @Test
+        fun `create Must return BAD_REQUEST When request body is not valid`() {
+            val notValidContent = """
+                {
+                    "userId": 123,
+                    "type": "telegram"
+                }
+            """.trimIndent()
+
+            mockMvc.post("/subscriptions/create") {
+                contentType = MediaType.APPLICATION_JSON
+                content = notValidContent
+            }
+                .andDo { print() }
+                .andExpectAll {
+                    status { isBadRequest() }
+                    content { string(containsString(JSON_PARSE_ERROR)) }
+                }
+
+            verify(exactly = 0) {
+                subscriptionService.createSubscription(
+                    userId = any(),
+                    subscriptionType = any(),
+                    filters = any(),
+                )
+            }
+        }
+    }
+
+    @Nested
+    inner class RemoveTest {
+
+        @Test
+        fun `remove Must return ok When ok`() {
+            val removeSubscriptionDto = randomRemoveSubscriptionDto.copy(type = SubscriptionType.TELEGRAM)
+
+            every {
+                subscriptionService.removeSubscription(
+                    userId = removeSubscriptionDto.userId,
+                    subscriptionType = removeSubscriptionDto.type,
+                )
+            } returns Unit
+
+            mockMvc.delete("/subscriptions/${removeSubscriptionDto.userId}/telegram")
+                .andDo { print() }
+                .andExpectAll {
+                    status { isOk() }
+                }
+
+            verify {
+                subscriptionService.removeSubscription(
+                    userId = removeSubscriptionDto.userId,
+                    subscriptionType = removeSubscriptionDto.type,
+                )
+            }
+        }
+
+        @Test
+        fun `remove Must return NOT_FOUND When request body is not valid`() {
+            mockMvc.delete("/subscriptions//sms")
+                .andDo { print() }
+                .andExpectAll {
+                    status { isNotFound() }
+                }
+
+            verify(exactly = 0) {
+                subscriptionService.createSubscription(
+                    userId = any(),
+                    subscriptionType = any(),
+                    filters = any(),
+                )
+            }
+        }
+    }
+
+    companion object {
+        const val JSON_PARSE_ERROR = "JSON parse error"
+    }
+}
